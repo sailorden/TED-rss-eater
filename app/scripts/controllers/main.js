0123989e84995e62de10f02891e45173e47677ee
Theodore <theo@Theodores-MacBook-Pro.local>
2014-05-31 00:50:35 -0700

    pre converting fully to ui-router

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,24 @@
 'use strict';
 
 angular.module('tedApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $resource) {
+    $scope.feed = [];
+    var Feed = $resource('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http%3A//feeds.feedburner.com/TEDTalks_video',
+      {}, {
+        fetch: {
+          method: 'JSONP',
+          params: {
+            v: '1.0',
+            callback: 'JSON_CALLBACK'
+          }
+        }
+      }
+    );
+
+    Feed.fetch()
+      .$promise.then(function(data) {
+        console.log(data.responseData.feed.entries);
+        $scope.feed = data.responseData.feed.entries;
+      });
+
   });
